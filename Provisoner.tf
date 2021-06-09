f78a73b7ece58d0b8724007a5d208e3a940606b0
Rajeshwarishivam <58301121+Rajeshwarishivam@users.noreply.github.com>
2021-06-09 17:56:42 +0530

    Create Provisoner.tf

---
+++ Provisoner.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web"{
+  ami   = "ami-081bb417559035fe8"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1b"
+  associate_public_ip_address = "true"
+  key_name = "AWSAutomation"
+  tags = {
+
+    Name = "webserver"
+  }
+   provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 775 /tmp/script.sh",
+      "sudo /tmp/script.sh",
+    ]
+  }
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = "${aws_instance.web.public_ip}"
+    private_key = file("AWSAutomation.pem")
+    agent = false
+    timeout = "2m"
+  }
+}
